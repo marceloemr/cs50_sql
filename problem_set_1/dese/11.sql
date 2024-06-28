2947a3f35eb4ee1c60ba3484c1f4eaadd3c4fe4f
Marcelo Eduardo Marques Ribas <contato.marcelomk@hotmail.com>
2024-06-28 12:05:28 -0300

    Add solution to pset1/dese/11

---
+++ problem_set_1/dese/11.sql
@@ -0,0 +1,7 @@
+SELECT "name", "graduated", "per_pupil_expenditure"
+FROM "schools"
+JOIN "graduation_rates"
+    ON "graduation_rates"."school_id" = "schools"."id"
+JOIN "expenditures"
+    ON "expenditures"."district_id" = "schools"."district_id"
+ORDER BY "per_pupil_expenditure" DESC, "schools"."name";
